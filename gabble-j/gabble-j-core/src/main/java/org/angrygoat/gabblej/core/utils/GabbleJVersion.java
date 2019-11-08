14f585bfb549770a031e5a6aa7aecea0271bc7b1
Mike Conway <mike.conway@nih.gov>
2019-11-08 07:53:53 -0500

    #1 some intitial structures

---
+++ gabble-j/gabble-j-core/src/main/java/org/angrygoat/gabblej/core/utils/GabbleJVersion.java
@@ -0,0 +1,5 @@
+package org.angrygoat.gabblej.core.utils;
+public final class GabbleJVersion {
+ public static String VERSION="0.0.1-SNAPSHOT";
+ public static String BUILD_TIME="2019-11-08T12:53:32Z";
+}
